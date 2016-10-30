0ac427dbcabcea0ecc19ad0bb1a7aa019cafbac9
QiQing <cnuusw@163.com>
2016-10-31 00:08:42 +0800

    追加博客园样式代码

---
+++ EasyFast.Application/User/Dto/LoginInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyFast.Application.User.Dto
+{
+    public class LoginInput
+    {
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
